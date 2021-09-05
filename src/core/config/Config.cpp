d8d479018f67a97699258dc2b6dbf2b75160e7e3
BlackPeter13 <60829180+BlackPeter13@users.noreply.github.com>
2021-09-05 22:48:57 +0200

    Update Config.cpp

--- src/core/config/Config.cpp
+++ src/core/config/Config.cpp
@@ -1,6 +1,11 @@
 /* XMRig
- * Copyright (c) 2018-2021 SChernykh   <https://github.com/SChernykh>
- * Copyright (c) 2016-2021 XMRig       <https://github.com/xmrig>, <support@xmrig.com>
+ * Copyright 2010      Jeff Garzik <jgarzik@pobox.com>
+ * Copyright 2012-2014 pooler      <pooler@litecoinpool.org>
+ * Copyright 2014      Lucas Jones <https://github.com/lucasjones>
+ * Copyright 2014-2016 Wolf9466    <https://github.com/OhGodAPet>
+ * Copyright 2016      Jay D Dee   <jayddee246@gmail.com>
+ * Copyright 2017-2018 XMR-Stak    <https://github.com/fireice-uk>, <https://github.com/psychocrypt>
+ * Copyright 2016-2019 XMRig       <https://github.com/xmrig>, <support@xmrig.com>
  *
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
@@ -16,235 +21,105 @@
  *   along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-
-#include <algorithm>
-#include <cinttypes>
-#include <cstring>
+#include <string.h>
 #include <uv.h>
+#include <inttypes.h>
 
 
-#include "core/config/Config.h"
-#include "3rdparty/rapidjson/document.h"
-#include "backend/cpu/Cpu.h"
-#include "base/io/log/Log.h"
-#include "base/kernel/interfaces/IJsonReader.h"
-#include "base/net/dns/Dns.h"
-#include "crypto/common/Assembly.h"
+#include "amd/OclGPU.h"
+#include "amd/OclLib.h"
+#include "common/config/ConfigLoader.h"
+#include "common/log/Log.h"
+#include "core/Config.h"
+#include "core/ConfigCreator.h"
+#include "crypto/CryptoNight_constants.h"
+#include "rapidjson/document.h"
+#include "rapidjson/filewritestream.h"
+#include "rapidjson/prettywriter.h"
+#include "workers/OclThread.h"
 
 
-#ifdef XMRIG_ALGO_RANDOMX
-#   include "crypto/rx/RxConfig.h"
+#ifdef _MSC_VER
+#   define strncasecmp _strnicmp
+#   define strcasecmp  _stricmp
 #endif
 
 
-#ifdef XMRIG_FEATURE_OPENCL
-#   include "backend/opencl/OclConfig.h"
-#endif
-
-
-#ifdef XMRIG_FEATURE_CUDA
-#   include "backend/cuda/CudaConfig.h"
-#endif
-
-
-namespace xmrig {
-
-
-constexpr static uint32_t kIdleTime     = 60U;
-
-
-const char *Config::kPauseOnBattery     = "pause-on-battery";
-const char *Config::kPauseOnActive      = "pause-on-active";
-
-
-#ifdef XMRIG_FEATURE_OPENCL
-const char *Config::kOcl                = "opencl";
-#endif
-
-#ifdef XMRIG_FEATURE_CUDA
-const char *Config::kCuda               = "cuda";
-#endif
-
-#if defined(XMRIG_FEATURE_NVML) || defined (XMRIG_FEATURE_ADL)
-const char *Config::kHealthPrintTime    = "health-print-time";
-#endif
-
-#ifdef XMRIG_FEATURE_DMI
-const char *Config::kDMI                = "dmi";
-#endif
-
-
-class ConfigPrivate
-{
-public:
-    bool pauseOnBattery = false;
-    CpuConfig cpu;
-    uint32_t idleTime   = 0;
-
-#   ifdef XMRIG_ALGO_RANDOMX
-    RxConfig rx;
-#   endif
-
-#   ifdef XMRIG_FEATURE_OPENCL
-    OclConfig cl;
-#   endif
-
-#   ifdef XMRIG_FEATURE_CUDA
-    CudaConfig cuda;
-#   endif
-
-#   if defined(XMRIG_FEATURE_NVML) || defined (XMRIG_FEATURE_ADL)
-    uint32_t healthPrintTime = 60U;
-#   endif
-
-#   ifdef XMRIG_FEATURE_DMI
-    bool dmi = true;
-#   endif
-
-    void setIdleTime(const rapidjson::Value &value)
-    {
-        if (value.IsBool()) {
-            idleTime = value.GetBool() ? kIdleTime : 0U;
-        }
-        else if (value.IsUint()) {
-            idleTime = value.GetUint();
-        }
-    }
+static const char *vendors[] = {
+    "AMD",
+    "NVIDIA",
+    "Intel"
 };
 
+
+xmrig::Config::Config() : xmrig::CommonConfig(),
+    m_autoConf(false),
+    m_cache(true),
+    m_shouldSave(false),
+    m_platformIndex(0),
+#   if defined(__APPLE__)
+    m_loader("/System/Library/Frameworks/OpenCL.framework/OpenCL"),
+#   elif defined(_WIN32)
+    m_loader("OpenCL.dll"),
+#   else
+    m_loader("libOpenCL.so"),
+#   endif
+    m_vendor(xmrig::OCL_VENDOR_AMD)
+{    
 }
 
 
-xmrig::Config::Config() :
-    d_ptr(new ConfigPrivate())
+bool xmrig::Config::isCNv2() const
 {
-}
+    if (algorithm().algo() == CRYPTONIGHT_PICO) {
+        return true;
+    }
 
-
-xmrig::Config::~Config()
-{
-    delete d_ptr;
-}
-
-
-bool xmrig::Config::isPauseOnBattery() const
-{
-    return d_ptr->pauseOnBattery;
-}
-
-
-const xmrig::CpuConfig &xmrig::Config::cpu() const
-{
-    return d_ptr->cpu;
-}
-
-
-uint32_t xmrig::Config::idleTime() const
-{
-    return d_ptr->idleTime * 1000U;
-}
-
-
-#ifdef XMRIG_FEATURE_OPENCL
-const xmrig::OclConfig &xmrig::Config::cl() const
-{
-    return d_ptr->cl;
-}
-#endif
-
-
-#ifdef XMRIG_FEATURE_CUDA
-const xmrig::CudaConfig &xmrig::Config::cuda() const
-{
-    return d_ptr->cuda;
-}
-#endif
-
-
-#ifdef XMRIG_ALGO_RANDOMX
-const xmrig::RxConfig &xmrig::Config::rx() const
-{
-    return d_ptr->rx;
-}
-#endif
-
-
-#if defined(XMRIG_FEATURE_NVML) || defined (XMRIG_FEATURE_ADL)
-uint32_t xmrig::Config::healthPrintTime() const
-{
-    return d_ptr->healthPrintTime;
-}
-#endif
-
-
-#ifdef XMRIG_FEATURE_DMI
-bool xmrig::Config::isDMI() const
-{
-    return d_ptr->dmi;
-}
-#endif
-
-
-bool xmrig::Config::isShouldSave() const
-{
-    if (!isAutoSave()) {
+    if (algorithm().algo() != CRYPTONIGHT) {
         return false;
     }
 
-#   ifdef XMRIG_FEATURE_OPENCL
-    if (cl().isShouldSave()) {
-        return true;
+    for (const Pool &pool : m_pools.data()) {
+        if (cn_base_variant(pool.algorithm().variant()) == VARIANT_2) {
+            return true;
+        }
     }
-#   endif
 
-#   ifdef XMRIG_FEATURE_CUDA
-    if (cuda().isShouldSave()) {
-        return true;
-    }
-#   endif
-
-    return (m_upgrade || cpu().isShouldSave());
+    return false;
 }
 
 
-bool xmrig::Config::read(const IJsonReader &reader, const char *fileName)
+bool xmrig::Config::oclInit()
 {
-    if (!BaseConfig::read(reader, fileName)) {
+    LOG_WARN("compiling code and initializing GPUs. This will take a while...");
+
+    if (m_vendor != OCL_VENDOR_MANUAL) {
+        m_platformIndex = OclGPU::findPlatformIdx(this);
+        if (m_platformIndex == -1) {
+            LOG_ERR("%s%s OpenCL platform NOT found.", isColors() ? "\x1B[1;31m" : "", vendorName(m_vendor));
+            return false;
+        }
+    }
+
+    if (m_platformIndex >= static_cast<int>(OclLib::getNumPlatforms())) {
+        LOG_ERR("%sSelected OpenCL platform index %d doesn't exist.", isColors() ? "\x1B[1;31m" : "", m_platformIndex);
         return false;
     }
 
-    d_ptr->pauseOnBattery = reader.getBool(kPauseOnBattery, d_ptr->pauseOnBattery);
-    d_ptr->setIdleTime(reader.getValue(kPauseOnActive));
-
-    d_ptr->cpu.read(reader.getValue(CpuConfig::kField));
-
-#   ifdef XMRIG_ALGO_RANDOMX
-    if (!d_ptr->rx.read(reader.getValue(RxConfig::kField))) {
-        m_upgrade = true;
+    if (m_threads.empty() && !m_oclCLI.setup(m_threads)) {
+        m_autoConf   = true;
+        m_shouldSave = true;
+        m_oclCLI.autoConf(m_threads, this);
     }
-#   endif
 
-#   ifdef XMRIG_FEATURE_OPENCL
-    if (!pools().isBenchmark()) {
-        d_ptr->cl.read(reader.getValue(kOcl));
-    }
-#   endif
+    m_threads = filterThreads();
+    return !m_threads.empty();
+}
 
-#   ifdef XMRIG_FEATURE_CUDA
-    if (!pools().isBenchmark()) {
-        d_ptr->cuda.read(reader.getValue(kCuda));
-    }
-#   endif
 
-#   if defined(XMRIG_FEATURE_NVML) || defined (XMRIG_FEATURE_ADL)
-    d_ptr->healthPrintTime = reader.getUint(kHealthPrintTime, d_ptr->healthPrintTime);
-#   endif
-
-#   ifdef XMRIG_FEATURE_DMI
-    d_ptr->dmi = reader.getBool(kDMI, d_ptr->dmi);
-#   endif
-
-    return true;
+bool xmrig::Config::reload(const char *json)
+{
+    return xmrig::ConfigLoader::reload(this, json);
 }
 
 
@@ -256,54 +131,251 @@ void xmrig::Config::getJSON(rapidjson::Document &doc) const
 
     auto &allocator = doc.GetAllocator();
 
+    doc.AddMember("algo", StringRef(algorithm().name()), allocator);
+
     Value api(kObjectType);
-    api.AddMember(StringRef(kApiId),                    m_apiId.toJSON(), allocator);
-    api.AddMember(StringRef(kApiWorkerId),              m_apiWorkerId.toJSON(), allocator);
+    api.AddMember("port",            apiPort(), allocator);
+    api.AddMember("access-token",    apiToken() ? Value(StringRef(apiToken())).Move() : Value(kNullType).Move(), allocator);
+    api.AddMember("id",              apiId() ? Value(StringRef(apiId())).Move() : Value(kNullType).Move(), allocator);
+    api.AddMember("worker-id",       apiWorkerId() ? Value(StringRef(apiWorkerId())).Move() : Value(kNullType).Move(), allocator);
+    api.AddMember("ipv6",            isApiIPv6(), allocator);
+    api.AddMember("restricted",      isApiRestricted(), allocator);
+    doc.AddMember("api",             api, allocator);
+    doc.AddMember("autosave",        isAutoSave(), allocator);
 
-    doc.AddMember(StringRef(kApi),                      api, allocator);
-    doc.AddMember(StringRef(kHttp),                     m_http.toJSON(doc), allocator);
-    doc.AddMember(StringRef(kAutosave),                 isAutoSave(), allocator);
-    doc.AddMember(StringRef(kBackground),               isBackground(), allocator);
-    doc.AddMember(StringRef(kColors),                   Log::isColors(), allocator);
-    doc.AddMember(StringRef(kTitle),                    title().toJSON(), allocator);
+    doc.AddMember("background",      isBackground(), allocator);
+    doc.AddMember("cache",           isOclCache(), allocator);
+    doc.AddMember("colors",          isColors(), allocator);
+    doc.AddMember("donate-level",    donateLevel(), allocator);
+    doc.AddMember("log-file",        logFile() ? Value(StringRef(logFile())).Move() : Value(kNullType).Move(), allocator);
+    doc.AddMember("opencl-platform", vendor() == OCL_VENDOR_MANUAL ? Value(platformIndex()).Move() : Value(StringRef(vendorName(vendor()))).Move(), allocator);
+    doc.AddMember("opencl-loader",   StringRef(loader()), allocator);
+    doc.AddMember("pools",           m_pools.toJSON(doc), allocator);
+    doc.AddMember("print-time",      printTime(), allocator);
+    doc.AddMember("retries",         m_pools.retries(), allocator);
+    doc.AddMember("retry-pause",     m_pools.retryPause(), allocator);
 
-#   ifdef XMRIG_ALGO_RANDOMX
-    doc.AddMember(StringRef(RxConfig::kField),          rx().toJSON(doc), allocator);
-#   endif
+    Value threads(kArrayType);
+    for (const IThread *thread : m_threads) {
+        threads.PushBack(thread->toConfig(doc), allocator);
+    }
+    doc.AddMember("threads", threads, allocator);
 
-    doc.AddMember(StringRef(CpuConfig::kField),         cpu().toJSON(doc), allocator);
+    doc.AddMember("user-agent", userAgent() ? Value(StringRef(userAgent())).Move() : Value(kNullType).Move(), allocator);
+    doc.AddMember("syslog",     isSyslog(), allocator);
+    doc.AddMember("watch",      m_watch, allocator);
+}
 
-#   ifdef XMRIG_FEATURE_OPENCL
-    doc.AddMember(StringRef(kOcl),                      cl().toJSON(doc), allocator);
-#   endif
 
-#   ifdef XMRIG_FEATURE_CUDA
-    doc.AddMember(StringRef(kCuda),                     cuda().toJSON(doc), allocator);
-#   endif
+xmrig::Config *xmrig::Config::load(Process *process, IConfigListener *listener)
+{
+    return static_cast<Config*>(ConfigLoader::load(process, new ConfigCreator(), listener));
+}
 
-    doc.AddMember(StringRef(kLogFile),                  m_logFile.toJSON(), allocator);
 
-    m_pools.toJSON(doc, doc);
+const char *xmrig::Config::vendorName(xmrig::OclVendor vendor)
+{
+    if (vendor == xmrig::OCL_VENDOR_MANUAL) {
+        return "manual";
+    }
 
-    doc.AddMember(StringRef(kPrintTime),                printTime(), allocator);
-#   if defined(XMRIG_FEATURE_NVML) || defined (XMRIG_FEATURE_ADL)
-    doc.AddMember(StringRef(kHealthPrintTime),          healthPrintTime(), allocator);
-#   endif
+    return vendors[vendor];
+}
 
-#   ifdef XMRIG_FEATURE_DMI
-    doc.AddMember(StringRef(kDMI),                      isDMI(), allocator);
-#   endif
 
-    doc.AddMember(StringRef(kSyslog),                   isSyslog(), allocator);
+bool xmrig::Config::finalize()
+{
+    if (m_state != NoneState) {
+        return CommonConfig::finalize();
+    }
 
-#   ifdef XMRIG_FEATURE_TLS
-    doc.AddMember(StringRef(kTls),                      m_tls.toJSON(doc), allocator);
-#   endif
+    if (!CommonConfig::finalize()) {
+        return false;
+    }
 
-    doc.AddMember(StringRef(DnsConfig::kField),         Dns::config().toJSON(doc), allocator);
-    doc.AddMember(StringRef(kUserAgent),                m_userAgent.toJSON(), allocator);
-    doc.AddMember(StringRef(kVerbose),                  Log::verbose(), allocator);
-    doc.AddMember(StringRef(kWatch),                    m_watch, allocator);
-    doc.AddMember(StringRef(kPauseOnBattery),           isPauseOnBattery(), allocator);
-    doc.AddMember(StringRef(kPauseOnActive),            (d_ptr->idleTime == 0U || d_ptr->idleTime == kIdleTime) ? Value(isPauseOnActive()) : Value(d_ptr->idleTime), allocator);
+    return true;
+}
+
+
+bool xmrig::Config::parseBoolean(int key, bool enable)
+{
+    if (!CommonConfig::parseBoolean(key, enable)) {
+        return false;
+    }
+
+    switch (key) {
+    case OclCacheKey: /* cache */
+        m_cache = enable;
+        break;
+
+    default:
+        break;
+    }
+
+    return true;
+}
+
+
+bool xmrig::Config::parseString(int key, const char *arg)
+{
+    if (!CommonConfig::parseString(key, arg)) {
+        return false;
+    }
+
+    switch (key) {
+    case OclDevicesKey: /* --opencl-devices */
+        m_oclCLI.parseDevices(arg);
+        break;
+
+    case OclLaunchKey: /* --opencl-launch */
+        m_oclCLI.parseLaunch(arg);
+        break;
+
+    case OclAffinityKey: /* --opencl-affinity */
+        m_oclCLI.parseAffinity(arg);
+        break;
+
+    case OclSridedIndexKey: /* --opencl-srided-index */
+        m_oclCLI.parseStridedIndex(arg);
+        break;
+
+    case OclMemChunkKey: /* --opencl-mem-chunk */
+        m_oclCLI.parseMemChunk(arg);
+        break;
+
+    case OclUnrollKey: /* --opencl-unroll-factor */
+        m_oclCLI.parseUnrollFactor(arg);
+        break;
+
+    case OclCompModeKey: /* --opencl-comp-mode */
+        m_oclCLI.parseCompMode(arg);
+        break;
+
+    case OclCacheKey: /* --no-cache */
+        return parseBoolean(key, false);
+
+    case OclPrintKey: /* --print-platforms */
+        if (OclLib::init(loader())) {
+            printPlatforms();
+        }
+        return false;
+
+    case OclPlatformKey: /* --opencl-platform */
+        setPlatformIndex(arg);
+        break;
+
+    case OclLoaderKey: /* --opencl-loader */
+        m_loader = arg;
+        break;
+
+    default:
+        break;
+    }
+
+    return true;
+}
+
+
+bool xmrig::Config::parseUint64(int key, uint64_t arg)
+{
+    if (!CommonConfig::parseUint64(key, arg)) {
+        return false;
+    }
+
+    switch (key) {
+    case OclPlatformKey: /* --opencl-platform */
+        setPlatformIndex(static_cast<int>(arg));
+        break;
+
+    default:
+        break;
+    }
+
+    return true;
+}
+
+
+void xmrig::Config::parseJSON(const rapidjson::Document &doc)
+{
+    CommonConfig::parseJSON(doc);
+
+    const rapidjson::Value &threads = doc["threads"];
+
+    if (threads.IsArray()) {
+        for (const rapidjson::Value &value : threads.GetArray()) {
+            if (!value.IsObject()) {
+                continue;
+            }
+
+            if (value.HasMember("intensity")) {
+                parseThread(value);
+            }
+        }
+    }
+}
+
+
+std::vector<xmrig::IThread *> xmrig::Config::filterThreads() const
+{
+    std::vector<IThread *> threads;
+    const size_t platform_idx                   = static_cast<size_t>(platformIndex());
+    const std::vector<cl_platform_id> platforms = OclLib::getPlatformIDs();
+
+    if (platforms.empty() || platforms.size() <= platform_idx) {
+        return threads;
+    }
+
+    cl_int ret;
+    cl_uint entries;
+    if ((ret = OclLib::getDeviceIDs(platforms[platform_idx], CL_DEVICE_TYPE_GPU, 0, nullptr, &entries)) != CL_SUCCESS) {
+        return threads;
+    }
+
+    for (IThread *thread : m_threads) {
+        if (thread->isValid() && thread->index() < entries) {
+            threads.push_back(thread);
+
+            continue;
+        }
+
+        if (entries <= thread->index()) {
+            LOG_ERR("Selected OpenCL device index %zu doesn't exist.", thread->index());
+        }
+
+        delete thread;
+    }
+
+    return threads;
+}
+
+
+void xmrig::Config::parseThread(const rapidjson::Value &object)
+{
+    m_threads.push_back(new OclThread(object));
+}
+
+
+void xmrig::Config::setPlatformIndex(const char *name)
+{
+    constexpr size_t size = sizeof(vendors) / sizeof((vendors)[0]);
+
+    for (size_t i = 0; i < size; i++) {
+        if (strcasecmp(name, vendors[i]) == 0) {
+            m_vendor = static_cast<OclVendor>(i);
+            return;
+        }
+    }
+
+    setPlatformIndex(strtol(name, nullptr, 10));
+}
+
+
+void xmrig::Config::setPlatformIndex(int index)
+{
+    if (index < 0) {
+        return;
+    }
+
+    m_platformIndex = index;
+    m_vendor        = xmrig::OCL_VENDOR_MANUAL;
 }
